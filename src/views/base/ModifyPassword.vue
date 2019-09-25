72e10364737570c6e98c031f00d497d7b98f23e4
ht0510 <h1228449191@outlook.com>
2019-09-25 20:46:40 +0800

    修改密码

--- src/views/base/ModifyPassword.vue
+++ src/views/base/ModifyPassword.vue
@@ -1,13 +1,183 @@
+
 <template>
-<div id="ModifyPassword">
-    修改密码
-</div>
+
+    <div class="change">
+    <div class="nav">
+      <el-breadcrumb>
+        <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>
+          <a href="javascript:void(0);">基础数据</a>
+        </el-breadcrumb-item>
+        <el-breadcrumb-item>修改密码</el-breadcrumb-item>
+      </el-breadcrumb> 
+
+     </div> 
+<!-- <span style="float:left;"><i class="el-icon-edit"></i>修改密码</span>
+		<br/>
+		<br/>
+		<br/> -->
+
+    <el-form
+      :model="ruleForm"
+      status-icon
+      :rules="rules"
+      ref="ruleForm"
+      label-width="100px"
+      class="demo-ruleForm"
+    >
+      <el-form-item label="旧密码" prop="pass">
+        <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="新密码" prop="newPass">
+        <el-input type="password" v-model="ruleForm.newPass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="checkPass">
+        <el-input v-model="ruleForm.checkPass"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
+
 <script>
 export default {
-    
-}
+  data() {
+    var checkAge = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入新密码"));
+      } else {
+        callback();
+      }
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入旧密码"));
+      } else {
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.ruleForm.newPass) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      ruleForm: {
+        pass: "",
+        checkPass: "",
+        newPass: ""
+      },
+      rules: {
+        pass: [{ validator: validatePass, trigger: "blur", required: true }],
+        checkPass: [
+          { validator: validatePass2, trigger: "blur", required: true }
+        ],
+        newPass: [{ validator: checkAge, trigger: "blur", required: true }]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      var _this = this;
+      _this.$refs[formName].validate(valid => {
+        if (valid) {
+          _this
+            .$confirm("你确定修改密码吗, 是否继续?", "提示", {
+              confirmButtonText: "确定",
+              cancelButtonText: "取消",
+              type: "warning"
+            })
+            .then(() => {
+              _this.$axios
+                .get("/api/User/ModifyPassword", {
+                  params: {
+                    uid: sessionStorage.getItem("uid"),
+                    oldPassword: _this.ruleForm.pass, //旧密码
+                    newPassword: _this.ruleForm.newPass //新密码
+                  }
+                })
+                .then(res => {
+                  console.log(res.data);
+                  if (res.data.code == -1) {
+                    _this.$message({
+                      message: "系统异常",
+                      type: "error"
+                    });
+                  }
+                  if (res.data.code == -2) {
+                    _this.$message({
+                      message: "参数错误",
+                      type: "error"
+                    });
+                  }
+                  if (res.data.code == -3) {
+                    _this.$message({
+                      message: "旧密码错误",
+                      type: "error"
+                    });
+                  }
+                  if (res.data.code == 0) {
+                    _this.$message({
+                      message: "数据没有任何变化 ",
+                      type: "info"
+                    });
+                  }
+                  if (res.data.code == 1) {
+                    _this.$message({
+                      message:
+                        "修改密码成功,需重新登录,3秒后自动跳转到登录页面",
+                      type: "success"
+                    });
+                    sessionStorage.clear();
+                    setTimeout(function() {
+                      _this.$router.replace("/");
+                    }, 1000);
+                  }
+                })
+                .catch(error => {
+                  this.$message({
+                    message: "修改密码失败",
+                    type: "error"
+                  });
+                  console.log(error);
+                });
+            });
+        } else {
+          _this.$message({
+            message: "请输入正确的密码",
+            type: "warning"
+          });
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
 </script>
-<style lang="less" scoped>
 
+<style scoped lang='less'>
+.change {
+  .nav,
+  .el-breadcrumb {
+    height: 40px;
+    line-height: 40px;
+    padding: 0px 10px;
+  }
+  .nav {
+    border-bottom: 1px solid #f0f0f0;
+  }
+  form {
+    margin-top: 10px;
+  }
+}
 </style>
--- src/views/base/UserManage.vue
+++ src/views/base/UserManage.vue
@@ -1,13 +1,114 @@
 <template>
-<div id="TeacherManage">
-    用户角色
-</div>
-</template>
-<script>
-export default {
-    
-}
-</script>
-<style lang="less" scoped>
+  <el-table
+    :data="tableData"
+    style="width: 100%">
+    <el-table-column
+      label="#"
+      width="180">
+      <template slot-scope="scope">
+        <i class="el-icon-time"></i>
+        <span style="margin-left: 10px">{{ scope.row.date }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="角色"
+      width="180">
+      <template slot-scope="scope">
+        <el-popover trigger="hover" placement="top">
+          <p>姓名: {{ scope.row.name }}</p>
+          <p>住址: {{ scope.row.address }}</p>
+          <div slot="reference" class="name-wrapper">
+            <el-tag size="medium">{{ scope.row.name }}</el-tag>
+          </div>
+        </el-popover>
+      </template>
+    </el-table-column>
+    <el-table-column label="操作">
+      <template slot-scope="scope">
+        <el-button
+          size="mini"
+          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+        <el-button
+          size="mini"
+          type="danger"
+           @click="handleDelete(scope.$index, scope.row)">删除</el-button> 
 
-</style>
+<!-- <el-button type="text" @click="dialogVisible = true">点击打开 Dialog</el-button>
+<el-dialog
+  title="提示"
+  :visible.sync="dialogVisible"
+  width="30%"
+  :before-close="handleClose">
+  <span>这是一段信息</span>
+  <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+  </span>
+</el-dialog> -->
+
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+  
+
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+      }
+      // 对话框
+      //  dialogVisible: false
+    },
+    methods: {
+      handleEdit(index, row) {
+        console.log(index, row);
+      },
+      handleDelete(index, row) {
+        console.log(index, row);
+      }
+
+
+       
+    }
+ 
+
+  }
+
+
+// export default {
+    // data() {
+      // return {
+      //   dialogVisible: false
+      // };
+    // },
+  //   methods: {
+  //     handleClose(done){
+  //       this.$confirm('确认关闭？')
+  //         .then(_ => {
+  //           done();
+  //         })
+  //         .catch(_ => {});
+  //     }
+   
+  // };
+
+</script>
