568a764ad6c960ad1ddadf3c99572d276dd78609
unknown <1402570420@qq.com>
2019-10-07 10:35:01 +0800

    最新

--- src/views/test/TestPaperManage.vue
+++ src/views/test/TestPaperManage.vue
@@ -6,23 +6,34 @@
       <el-breadcrumb-item>试卷管理</el-breadcrumb-item>
     </el-breadcrumb>
     <el-card class="box-card">
-      <div class="text item">
-        <el-table :data="tableData" style="width: 100%" border=true>
-          <el-table-column label="#" prop="date"></el-table-column>
-          <el-table-column label="标题" prop="name"></el-table-column>
-          <el-table-column label="出卷人" prop="address"></el-table-column>
-          <el-table-column label="课程" prop="a"></el-table-column>
-          <el-table-column label="出卷日期" prop="b"></el-table-column>
+     <div slot="header" class="clearfix">
+        <el-table :data="tableData" style="width:100%" :border=true>
+          <el-table-column label="#" type="index"></el-table-column>
+          <el-table-column label="标题" prop="tpTitle"></el-table-column>
+          <el-table-column label="出卷人" prop="userName"></el-table-column>
+          <el-table-column label="课程" prop="courseName"></el-table-column>
+          <el-table-column label="出卷日期" prop="tpDate"></el-table-column>
           <el-table-column align="left" label="操作">
             <template slot-scope="scope">
               <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-               <el-button size="mini">详情</el-button>
+              <el-button size="mini">详情</el-button>
               <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-              
             </template>
           </el-table-column>
         </el-table>
-      </div>
+        <div class="text item">
+          <div class="block">
+            <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :page-size="10"
+                layout="total, sizes, prev, pager, next, jumper"
+              :total="pages"
+            ></el-pagination>
+          </div>
+        </div>
+        </div>
+     
     </el-card>
   </div>
 </template>
@@ -30,18 +41,11 @@
 export default {
   data() {
     return {
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-          a: "111",
-          b: "222",
-          c: "333"
-        },
-       
-      ],
-      search: ""
+      tableData: [],
+      pages:0,
+      each:10,
+
+    fewPages:1
     };
   },
   methods: {
@@ -50,8 +54,35 @@ export default {
     },
     handleDelete(index, row) {
       console.log(index, row);
-    }
+    },
+    handleSizeChange(val) {
+     var _this=this
+     _this.each = val
+    _this.testPaper()
+    },
+    handleCurrentChange(val) {
+         var _this = this
+         _this.fewPages=val
+         _this.testPaper()
+    },
+    testPaper(){
+        var _this=this
+        _this.axios("/api/TestPaper/GetTestPaperList?pageIndex="+_this.fewPages+"&pageSize="+_this.each).then(function(data){
+           _this.tableData=data.data.data
+           _this.pages=data.data.items
+          
+           console.log( _this.tableData)
+        })
+    } 
+
+  },
+  created(){
+    var _this=this
+    _this.testPaper()
   }
+
+
+
 };
 </script>
 <style lang="less" scoped>
@@ -59,4 +90,13 @@ export default {
 /deep/.el-breadcrumb {
   margin-bottom: 20px;
 }
+
+.block{
+  border-top: 1px solid #EBEEF5;
+  text-align: center;
+  height: 60px;
+  line-height: 60px;
+}
+
+
 </style>
