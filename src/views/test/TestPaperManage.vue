79304f71fa28329037779bbb113b646efd1707fb
unknown <1402570420@qq.com>
2019-10-09 16:54:43 +0800

    16:54

--- src/views/test/TestPaperManage.vue
+++ src/views/test/TestPaperManage.vue
@@ -21,9 +21,10 @@
           </el-table-column>
           <el-table-column align="left" label="操作">
             <template slot-scope="scope">
-              <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+              <!-- hasTest -->
+              <el-button size="mini" @click="handleEdit(scope.$index, scope.row)" :disabled="scope.row.hasTest">编辑</el-button>
               <el-button size="mini" @click="handleGet(scope.$index, scope.row)">详情</el-button>
-              <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+              <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)" :disabled="scope.row.hasTest">删除</el-button>
             </template>
           </el-table-column>
         </el-table>
