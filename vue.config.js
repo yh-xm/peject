bd9ec47639196f8d8f27016a95aca7d504b8ec94
JavaScript <3284885524@qq.com>
2019-10-15 10:57:17 +0800

    没代理

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
-module.exports = {
-    devServer:{
-        proxy:{
-            '/api':{
-                target:'http://192.168.1.188:12',
-                ws:true,
-                changeOrigin:true
-            }
-        }
-    }
+// module.exports = {
+//     devServer:{
+//         proxy:{
+//             '/api':{
+//                 target:'http://192.168.1.188:12',
+//                 ws:true,
+//                 changeOrigin:true
+//             }
+//         }
+//     }
 
-};
+// };
